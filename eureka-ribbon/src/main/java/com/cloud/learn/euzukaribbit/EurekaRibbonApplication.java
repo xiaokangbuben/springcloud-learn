ef7f89ffcb57e7733b54608359e80a5c62689340
xiaokang <489503330@qq.com>
2019-08-07 14:43:01 +0800

    test

---
+++ eureka-ribbon/src/main/java/com/cloud/learn/euzukaribbit/EurekaRibbonApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.learn.euzukaribbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaRibbonApplication.class, args);
+	}
+
+}
